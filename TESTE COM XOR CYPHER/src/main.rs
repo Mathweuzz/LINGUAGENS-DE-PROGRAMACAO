0a7d77ecc0ba9986020f40a80cf1a7d098ae0e5c
Mathweuzz <mathweuzz@gmail.com>
2023-02-03 14:36:44 -0300

    Update main.rs

--- TESTE COM XOR CYPHER/src/main.rs
+++ TESTE COM XOR CYPHER/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::prelude::*;
 use base64;
 
@@ -15,6 +16,7 @@ fn decrypt_xor(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
 fn main() {
     println!("1. Cadastrar usuário");
     println!("2. Autenticar usuário");
+    println!("3. Deletar usuário");
     println!("0. Sair");
 
     let mut opcao = String::new();
@@ -33,7 +35,7 @@ fn main() {
             let ciphertext = encrypt_xor(password.as_bytes(), SECRET_KEY.as_bytes());
             let encoded = base64::encode(ciphertext);
 
-            let mut file = File::create("usuarios.csv").unwrap();
+            let mut file = OpenOptions::new().append(true).open("usuarios.csv").unwrap();
             write!(file, "{},{}\n", username.trim(), encoded).unwrap();
         },
         2 => {
@@ -63,11 +65,48 @@ fn main() {
             }
             println!("Autenticação falhou");
         },
+        3 => {
+            println!("Insira o nome de usuário:");
+            let mut username = String::new();
+            std::io::stdin().read_line(&mut username).unwrap();
+
+            println!("Insira a senha:");
+            let mut password = String::new();
+            std::io::stdin().read_line(&mut password).unwrap();
+
+            let mut file = File::open("usuarios.csv").unwrap();
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).unwrap();
+
+            let mut lines = contents.lines().collect::<Vec<&str>>();
+            let mut found = false;
+            for i in 0..lines.len() {
+                let parts: Vec<&str> = lines[i].split(",").collect();
+                let user = parts[0];
+                let encoded_password = parts[1];
+                let ciphertext = base64::decode(encoded_password).unwrap();
+                let decrypted = decrypt_xor(&ciphertext, SECRET_KEY.as_bytes());
+
+                if username.trim() == user && password.as_bytes() == &decrypted[..] {
+                    found = true;
+                    lines.remove(i);
+                    break;
+                }
+            }
+            if found {
+                let new_contents = lines.join("\n");
+                let mut file = File::create("usuarios.csv").unwrap();
+                file.write_all(new_contents.as_bytes()).unwrap();
+                println!("Usuário deletado com sucesso");
+            } else {
+                println!("Autenticação falhou");
+            }
+        },
         0 => {
             println!("Saindo...");
             return;
         },
         _=> println!("Opção inválida")
-        }
     }
+}
 
